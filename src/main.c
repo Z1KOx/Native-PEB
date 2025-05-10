967652277e55665c2e3eccb26f433670cceb28f9
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2025-05-10 14:29:07 +0200

    [🛠️] Refactor assembly, switch to C, fix CMake

---
+++ src/main.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+PVOID ImageBaseAddress();
+PVOID ProcessHeap();
+ULONG NtGlobalFlag();
+ULONG OSMajorVersion();
+ULONG OSMinorVersion();
+USHORT OSBuildNumber();
+USHORT OSCSDVersion();
+ULONG OSPlatformId();
+ULONG ImageSubsystem();
+ULONG ImageSubsystemMajorVersion();
+ULONG ImageSubsystemMinorVersion();
+ULONG ProcessId();
+
+int main() {
+    puts("--------------------------------------------------");
+    printf("ImageBaseAddress:          0x%llx\n", ( unsigned long long )ImageBaseAddress());
+    printf("ProcessHeap:               0x%llx\n", ( unsigned long long )ProcessHeap());
+    printf("Global Flag:               0x%lx\n", NtGlobalFlag());
+    printf("Version:                   %lu.%lu.%u\n", OSMajorVersion(), OSMinorVersion(), OSBuildNumber());
+    printf("OSCSD Version:             %u\n", OSCSDVersion());
+    printf("OS Platform ID:            %lu\n", OSPlatformId());
+    printf("Image Subsystem:           %lu\n", ImageSubsystem());
+    printf("Image Subsystem Version:   %lu.%lu\n", ImageSubsystemMajorVersion(), ImageSubsystemMinorVersion());
+    printf("PID:                       %lu\n", ProcessId());
+    puts("--------------------------------------------------");
+
+    return 0;
+}
--- src/osinfo.asm
+++ src/osinfo.asm
@@ -3,123 +3,104 @@ PUBLIC ProcessHeap
 PUBLIC OSMajorVersion
 PUBLIC OSMinorVersion
 PUBLIC OSBuildNumber
-PUBLIC ProcessId
-PUBLIC NtGlobalFlag
 PUBLIC OSCSDVersion
 PUBLIC OSPlatformId
 PUBLIC ImageSubsystem
 PUBLIC ImageSubsystemMajorVersion
 PUBLIC ImageSubsystemMinorVersion
+PUBLIC ProcessId
+PUBLIC NtGlobalFlag
 PUBLIC fail_return_zero
 
+.data
+    PEB_BASE                equ 60h
+    PEB_IMAGE_BASE          equ 10h
+    PEB_PROCESS_HEAP        equ 30h
+    PEB_NT_GLOBAL_FLAG      equ 68h
+    PEB_OS_MAJOR_VERSION    equ 118h
+    PEB_OS_MINOR_VERSION    equ 11Ch
+    PEB_OS_BUILD_NUMBER     equ 120h
+    PEB_OS_CSD_VERSION      equ 122h
+    PEB_OS_PLATFORM_ID      equ 124h
+    PEB_IMAGE_SUBSYSTEM     equ 128h
+    PEB_SUBSYSTEM_MAJOR     equ 12Ch
+    PEB_SUBSYSTEM_MINOR     equ 130h
+    TEB_PROCESS_ID          equ 40h
+
 .code
+    ReadPebValue PROC
+        mov     rax, qword ptr gs:[PEB_BASE]
+        test    rax, rax
+        jz      fail_return_zero
+        mov     rax, [rax + rcx]
+        test    rax, rax
+        jz      fail_return_zero
+        ret
+    ReadPebValue ENDP
+
     ImageBaseAddress PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 10h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_IMAGE_BASE
+        jmp     ReadPebValue
     ImageBaseAddress ENDP
+
     ProcessHeap PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 30h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_PROCESS_HEAP
+        jmp     ReadPebValue
     ProcessHeap ENDP
+
+    NtGlobalFlag PROC
+        mov     rcx, PEB_NT_GLOBAL_FLAG
+        jmp     ReadPebValue
+    NtGlobalFlag ENDP
+
     OSMajorVersion PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 118h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_OS_MAJOR_VERSION
+        jmp     ReadPebValue
     OSMajorVersion ENDP
+
     OSMinorVersion PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 11Ch]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_OS_MINOR_VERSION
+        jmp     ReadPebValue
     OSMinorVersion ENDP
+
     OSBuildNumber PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 120h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_OS_BUILD_NUMBER
+        jmp     ReadPebValue
     OSBuildNumber ENDP
+
     OSCSDVersion PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 122h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_OS_CSD_VERSION
+        jmp     ReadPebValue
     OSCSDVersion ENDP
+
     OSPlatformId PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 124h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_OS_PLATFORM_ID
+        jmp     ReadPebValue
     OSPlatformId ENDP
+
     ImageSubsystem PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 128h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_IMAGE_SUBSYSTEM
+        jmp     ReadPebValue
     ImageSubsystem ENDP
+
     ImageSubsystemMajorVersion PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 12Ch]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_SUBSYSTEM_MAJOR
+        jmp     ReadPebValue
     ImageSubsystemMajorVersion ENDP
+
     ImageSubsystemMinorVersion PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 130h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
+        mov     rcx, PEB_SUBSYSTEM_MINOR
+        jmp     ReadPebValue
     ImageSubsystemMinorVersion ENDP
+
     ProcessId PROC
-        mov     rax,     qword ptr gs:[40h]
-        test    rax,     rax
+        mov     rax, qword ptr gs:[TEB_PROCESS_ID]
+        test    rax, rax
         jz      fail_return_zero
         ret
     ProcessId ENDP
-    NtGlobalFlag PROC
-        mov     rax,     qword ptr gs:[60h]
-        test    rax,     rax
-        jz      fail_return_zero
-        mov     rax,     [rax + 68h]
-        test    rax,     rax
-        jz      fail_return_zero
-        ret
-    NtGlobalFlag ENDP
 
     fail_return_zero:
-        xor     rax,     rax
+        xor     rax, rax
         ret
-end
+END
